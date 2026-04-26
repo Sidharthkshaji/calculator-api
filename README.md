566d3206673adb9303751b5fb824483d042522e7
Sidharth K Shaji <sidharthkshaji@gmail.com>
2026-04-26 14:33:21 +0530

    Built Calculator API using Flask

---
+++ README.md
@@ -0,0 +1,15 @@
+# Calculator API
+
+A simple Flask calculator API.
+
+## Endpoints
+
+/add?a=5&b=10
+/sub?a=10&b=5
+/mul?a=5&b=2
+/div?a=10&b=2
+/div?a=7&b=0
+/exp?a=2&b=2
+/rem?a=15&b=7
+
+Built as part of building my backend knowledge.
---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "This is a simple calculator"
+
+@app.route('/add')
+def add():
+    a = float(request.args.get('a',0))
+    b = float(request.args.get('b',0))
+    return jsonify({
+        "First Number " : a,
+        "Second Number " : b,
+        "Sum" : a+b
+    })
+@app.route('/sub')
+def sub():
+    a = float(request.args.get('a',0))
+    b = float(request.args.get('b',0))
+    return jsonify({
+        "First Number " : a,
+        "Second Number " : b,
+        "Difference" : a-b
+    })
+@app.route('/mul')
+def mul():
+    a = float(request.args.get('a',0))
+    b = float(request.args.get('b',0))
+    return jsonify({
+        "First number" : a,
+        "Second number" : b,
+        "product" : a*b
+    })
+@app.route('/div')
+def div():
+    a = float(request.args.get('a',0))
+    b = float(request.args.get('b',0))
+    if b==0 : return "Cannot divided by 0"
+    return jsonify({
+        "First number" : a,
+        "Second number" : b,
+        "quotient" : a/b
+    })
+@app.route('/exp')
+def exp():
+    a = float(request.args.get('a',0))
+    b = float(request.args.get('b',0))
+    return jsonify({
+        "First Number " : a,
+        "Second Number " : b,
+        "Exponentiation" : a**b
+    })
+@app.route('/rem')
+def rem():
+    a = float(request.args.get('a',0))
+    b = float(request.args.get('b',0))
+    return jsonify({
+        "First Number " : a,
+        "Second Number " : b,
+        "Reminder" : a%b
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
